677436deda15aa2a46c2bf1da514453f176b84f1
Krypton <madhusudanbabar@gmail.com>
2021-01-13 13:30:21 +0530

    updated flutter sdk and added questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:vscode_hw/answer.dart';
 
-void main() => runApp(MyApp());
+import 'package:vscode_hw/question.dart';
+
+void main() => runApp(
+      MaterialApp(
+        title: "Questionnaire",
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.pink),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Questionnaire"),
+            ),
+            body: MyApp()),
+      ),
+    );
 
 class MyApp extends StatefulWidget {
   @override
@@ -12,55 +24,53 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyApp extends State<MyApp> {
-  int counter = 0;
+  int currQuestion = 0;
+  var questions = [
+    {
+      "question": "what is the capital of india?",
+      "answers": ["mumbai", "delhi", "banglore", "chennai"],
+      "correctAnswer": "delhi"
+    },
+    {
+      "question": "what is the state bird of india?",
+      "answers": ["peacock", "owl", "sparrow", "crow"],
+      "correctAnswer": "peacock"
+    },
+    {
+      "question": "which of the following is not UPI app?",
+      "answers": ["whatsapp", "phonePe", "Google pay", "tinder"],
+      "correctAnswer": "tinder"
+    },
+  ];
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Counter demo app",
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.pink),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Counter demo app"),
-          automaticallyImplyLeading: false,
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () => debugPrint("hello world"),
-          ),
-          actions: <Widget>[
-            Padding(
-              child: IconButton(
-                icon: Icon(Icons.exit_to_app),
-                onPressed: () => exit(0)
-              ),
-              padding: EdgeInsets.only(right: 10),
-            )
-          ],
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          margin: EdgeInsets.all(10),
+          child: QuizApp((questions[currQuestion]["question"]) as String),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => setState(() {
-            counter++;
-          }),
-          tooltip: "add",
-          child: Icon(Icons.add),
-        ),
-        body: Container(
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "You've pressed button these many times",
-                style: TextStyle(color: Colors.pink[600]),
-              ),
-              Text(
-                counter.toString(),
-                style: Theme.of(context).textTheme.headline4,
-              )
-            ],
-          ),
-        ),
-      ),
+        ...(questions[currQuestion]["answers"] as List).map((opt) {
+          // TODO: implement correct answer handler and score card
+          return Answer(
+            opt,
+            getSnackBar,
+          );
+        }).toList()
+      ],
     );
+  }
+
+  Widget getSnackBar(String option) {
+    var sb = SnackBar(
+      content: Text("Answer $option selected"),
+    );
+    setState(() {
+      (currQuestion + 1) >= questions.length
+          ? currQuestion = 0
+          : currQuestion++;
+    });
+    return sb;
   }
 }
